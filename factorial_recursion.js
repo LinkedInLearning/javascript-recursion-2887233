382daccc5c7418d94ea5ac399960e373a15dfa93
Rufai Mustapha <etop94@gmail.com>
2021-06-10 10:05:43 +0100

    code refactoring

--- factorial_recursion.js
+++ factorial_recursion.js
@@ -1,9 +1,9 @@
-function recursive_factorial(index){
-	if (index === 0){
-		return 1
-	}else {
-		return index * recursive_factorial(index -1)
-	}
+function recursive_factorial(index) {
+    if (index === 0) {
+        return 1
+    } else {
+        return index * recursive_factorial(index - 1)
+    }
 }
 
 console.log(recursive_factorial(0))
@@ -14,6 +14,15 @@ console.log(recursive_factorial(3))
 console.log(recursive_factorial(4))
 console.log(recursive_factorial(5))
 
-console.time("factorial")
+
 console.log(recursive_factorial(50))
-console.timeEnd("factorial")
+
+
+const t0 = performance.now();
+
+console.log(recursive_factorial(5))
+
+const t1 = performance.now();
+console.log(`Time to complete operation ${
+    Math.round((t1 - t0) / 1000)
+} seconds.`);
